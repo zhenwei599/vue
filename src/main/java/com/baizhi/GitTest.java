c8d270bb5461257f95f2ca7ca45347d9ecd01ea1
lee <912348199@qq.com>
2020-08-13 20:30:34 +0800

    使用dev分支在test中添加了一行dev

--- src/main/java/com/baizhi/GitTest.java
+++ src/main/java/com/baizhi/GitTest.java
@@ -3,5 +3,6 @@ package com.baizhi;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("args = " + args);
+        System.out.println("-----dev分支-----");
     }
 }
