a6ef08ca31dce6ce296d111b2164d0a1986e4f89
lee <912348199@qq.com>
2020-08-13 21:05:07 +0800

    git 中定义了aa

--- src/main/java/com/baizhi/GitTest.java
+++ src/main/java/com/baizhi/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("args = " + args);
         System.out.println("-----dev分支-----");
+        int a = 10;
     }
 }
